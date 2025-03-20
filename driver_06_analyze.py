f45058a4bee65a3dec32b1ca7ab5292ab88fa9a3
Neil Ranly <neil.ranly@gmail.com>
2025-03-20 14:35:21 -0400

    Added search filter classification analysis logic

--- driver_06_analyze.py
+++ driver_06_analyze.py
@@ -10,15 +10,35 @@ db_file_path = 'data.db'
 
 with sqlite3.connect(db_file_path) as conn:
     df_results = pd.read_sql('''
-        SELECT model_id, classification_correct_result, extraction_correct_result, answer_value as has_events, COUNT(*) as instance_count FROM (
-        SELECT model_id, answer_value,
-        MAX(CASE WHEN (prompt_strategy == "DefaultEventExistsClassificationTaskPrompt")  THEN correct END) as classification_correct_result,
-        MAX(CASE WHEN (prompt_strategy == "DefaultExtractEventsPrompt") THEN correct END) as extraction_correct_result,
-        COUNT(*) as pt_count
-        FROM task_results GROUP BY model_id, task) GROUP BY model_id, classification_correct_result, extraction_correct_result, answer_value
-                ''',conn)
+        SELECT model_id, answer_value as has_events, text_filter_correct_result, classification_correct_result, extraction_correct_result, COUNT(*) as instance_count FROM (
+            SELECT tr.model_id as model_id, tr.answer_value as answer_value,
+                    ftr.correct as text_filter_correct_result,
+                    MAX(CASE WHEN (tr.prompt_strategy == "DefaultEventExistsClassificationTaskPrompt")  THEN tr.correct END) as classification_correct_result,
+                    COUNT(CASE WHEN (tr.prompt_strategy == "DefaultEventExistsClassificationTaskPrompt")  THEN 1 END) as classification_result_count,
+                    MAX(CASE WHEN (tr.prompt_strategy == "DefaultExtractEventsPrompt") THEN tr.correct END) as extraction_correct_result,
+                    COUNT(CASE WHEN (tr.prompt_strategy == "DefaultExtractEventsPrompt") THEN 1 END) as extraction_result_count,
+                    COUNT(*) as pt_count
+            FROM task_results as tr INNER JOIN (
+                SELECT * FROM task_results WHERE prompt_strategy == "full_text_search"
+            ) as ftr ON ftr.task = tr.task
+            WHERE tr.prompt_strategy != "full_text_search" 
+            GROUP BY tr.model_id, tr.task
+        )
+        GROUP BY model_id, answer_value, text_filter_correct_result, classification_correct_result, extraction_correct_result
+                ''', conn)
 
-print(df_results.head())
+    df_search_filter_counts = pd.read_sql('''
+        SELECT 
+            answer_value as has_events,
+            COUNT(CASE WHEN answer_response == "1" THEN answer_value == "1" END) as predicted_true,
+            COUNT(CASE WHEN answer_response == "0" THEN answer_value == "0" END) as predicted_false
+        FROM task_results WHERE prompt_strategy == "full_text_search" GROUP BY answer_value
+    ''', conn)
+
+print("Search Filter results:")
+print(df_search_filter_counts)
+
+#print(df_results.head())
 
 model_ids = df_results["model_id"].unique()
 
@@ -30,15 +50,26 @@ for model_id in model_ids:
     df_true = df_f[df_f['has_events'] == "1"]
     df_false = df_f[df_f['has_events'] == "0"]
     #print(df_true.head())
-    print("Classification Confusion Matrix")
+    print("LLM Classification Confusion Matrix")
     print(f"truth | {df_true[df_true['classification_correct_result'] == 1]['instance_count'].sum()} | {df_true[df_true['classification_correct_result'] == 0]['instance_count'].sum()}")
     print(f"false | {df_false[df_false['classification_correct_result'] == 0]['instance_count'].sum()} | {df_false[df_false['classification_correct_result'] == 1]['instance_count'].sum()}")
-
 
     print("Extraction Confusion Matrix")
     print(f"truth | {df_true[df_true['extraction_correct_result'] == 1]['instance_count'].sum()} | {df_true[df_true['extraction_correct_result'] == 0]['instance_count'].sum()}")
     print(f"false | {df_false[df_false['extraction_correct_result'] == 0]['instance_count'].sum()} | {df_false[df_false['extraction_correct_result'] == 1]['instance_count'].sum()}")
 
-    print("Combined Confusion Matrix")
+    print("C+E Confusion Matrix")
     print(f"truth | {df_true[(df_true['classification_correct_result'] == 1) & (df_true['extraction_correct_result'] == 1)]['instance_count'].sum()} | {df_true[(df_true['extraction_correct_result'] == 0) | (df_true['classification_correct_result'] == 0)]['instance_count'].sum()}")
     print(f"false | {df_false[(df_false['extraction_correct_result'] == 0) & (df_false['classification_correct_result'] == 0)]['instance_count'].sum()} | {df_false[(df_false['extraction_correct_result'] == 1) | (df_false['classification_correct_result'] == 1)]['instance_count'].sum()}")
+
+    print("T+E Confusion Matrix")
+    print(f"truth | {df_true[(df_true['text_filter_correct_result'] == 1) & (df_true['extraction_correct_result'] == 1)]['instance_count'].sum()} | {df_true[(df_true['extraction_correct_result'] == 0) | (df_true['text_filter_correct_result'] == 0)]['instance_count'].sum()}")
+    print(f"false | {df_false[(df_false['extraction_correct_result'] == 0) & (df_false['text_filter_correct_result'] == 0)]['instance_count'].sum()} | {df_false[(df_false['extraction_correct_result'] == 1) | (df_false['text_filter_correct_result'] == 1)]['instance_count'].sum()}")
+
+    print("C+T Confusion Matrix")
+    print(f"truth | {df_true[(df_true['classification_correct_result'] == 1) & (df_true['text_filter_correct_result'] == 1)]['instance_count'].sum()} | {df_true[(df_true['text_filter_correct_result'] == 0) | (df_true['classification_correct_result'] == 0)]['instance_count'].sum()}")
+    print(f"false | {df_false[(df_false['text_filter_correct_result'] == 0) & (df_false['classification_correct_result'] == 0)]['instance_count'].sum()} | {df_false[(df_false['text_filter_correct_result'] == 1) | (df_false['classification_correct_result'] == 1)]['instance_count'].sum()}")
+
+    print("All Confusion Matrix")
+    print(f"truth | {df_true[(df_true['text_filter_correct_result'] == 1) & (df_true['classification_correct_result'] == 1) & (df_true['extraction_correct_result'] == 1)]['instance_count'].sum()} | {df_true[(df_true['text_filter_correct_result'] == 0) | (df_true['extraction_correct_result'] == 0) | (df_true['classification_correct_result'] == 0)]['instance_count'].sum()}")
+    print(f"false | {df_false[(df_false['text_filter_correct_result'] == 0) & (df_false['extraction_correct_result'] == 0) & (df_false['classification_correct_result'] == 0)]['instance_count'].sum()} | {df_false[(df_false['text_filter_correct_result'] == 1) | (df_false['extraction_correct_result'] == 1) | (df_false['classification_correct_result'] == 1)]['instance_count'].sum()}")
--- driver_08_search_filter.py
+++ driver_08_search_filter.py
@@ -1,6 +1,7 @@
 import typing
 
 from driver_00_setup import event_types, datasets, keywords
+from driver_01_prep_db import database
 from study_llm.event_extraction_task.dataset_maven import read_in_dataset, MavenParagraph
 
 from study_llm.full_text_search_db import FullTextSearch
@@ -13,5 +14,63 @@ search_db = FullTextSearch()
 
 results = search_db.search(" OR ".join(keywords))
 
-print(results)
-print(len(results))
+print(f"Found {len(results)} results")
+
+result_map = {}
+for r in results:
+    title = r[0]
+    if title in result_map:
+        raise ValueError("Didn't expect multiple pieces of content with the same title for the computations below")
+    
+    result_map[title] = r
+
+found_count = 0
+unfound_map = {}
+
+for dataset_file_path in datasets:
+    extraction_tasks:typing.List[MavenParagraph] = read_in_dataset(
+        dataset_file_path,
+        event_types=event_types
+    )
+
+    for et in extraction_tasks:
+
+        if et.title() in result_map:
+            found_count += 1
+            result_map[et.title()] = et
+        else:
+            unfound_map[et.title()] = et
+
+print(f"Re-mapped to extraction task structure count: {found_count}")
+
+if __name__ == "__main__":
+    model_text_search_id = "text_search"
+    database.add_model_entry(
+        model_id=model_text_search_id,
+        model_name="Full Text Search",
+        model_size=0,
+        model_version=1,
+    )
+
+    for _, et in result_map.items():
+        database.add_task_result(
+            task=et,
+            log="search",
+            prompt_strategy="full_text_search",
+            model_id=model_text_search_id,
+            discussion_duration=0.0,
+            answer_value="1" if len(et.events) > 0 else "0",
+            answer_response="1",
+            correct=1 if len(et.events) > 0 else 0
+        )
+    for _, et in unfound_map.items():
+        database.add_task_result(
+            task=et,
+            log="search",
+            prompt_strategy="full_text_search",
+            model_id=model_text_search_id,
+            discussion_duration=0.0,
+            answer_value="1" if len(et.events) > 0 else "0",
+            answer_response="0",
+            correct=0 if len(et.events) > 0 else 1
+        )
